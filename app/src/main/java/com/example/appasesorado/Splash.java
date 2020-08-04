66dd619b646b920ed0a1eada76984545133fb40f
christiancervantes <chriscervantesdelacruz@gmail.com>
2020-08-04 15:22:44 -0500

    tercer commit

--- app/src/main/java/com/example/appasesorado/Splash.java
+++ app/src/main/java/com/example/appasesorado/Splash.java
@@ -1,59 +1,144 @@
 package com.example.appasesorado;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.appasesorado.Comun.Comun;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.firebase.ui.auth.AuthMethodPickerLayout;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.security.Principal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Completable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Action;
 
 public class Splash extends AppCompatActivity {
 
+    private static final int LOGIN_REQUEST_CODE = 7171;
+
+    //autenficacion
+    private List<AuthUI.IdpConfig> providers;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener listener;
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        delaySplashScreen();
+    }
+
+    @Override
+    protected void onStop() {
+        if (firebaseAuth != null && listener != null)
+            firebaseAuth.removeAuthStateListener(listener);
+        super.onStop();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
+    init();
 
-        ImageView logo = findViewById(R.id.logo);
-
-        if (Comun.isConnectedToInternet(getBaseContext())){
-            Animation myanim = AnimationUtils.loadAnimation(this,R.anim.animacion1);
-
-            logo.startAnimation(myanim);
-
-            final Intent intent = new Intent(Splash.this, Login.class);
-            Thread timer = new Thread(){
-                public void run (){
-                    try{
-                        sleep(2000);
-                    }catch (InterruptedException e){
-                        e.printStackTrace();
-                    }
-                    finally {
-                        startActivity(intent);
-                        finish();
-                    }
-                }
-            };
-            timer.start();
-
-        }
-        else{
-            Toast.makeText(Splash.this,"Revise su conexion a internet !!",Toast.LENGTH_LONG).show();
-            return;
-        }
     }
 
 
+
+    private void init() {
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+               //,new AuthUI.IdpConfig.FacebookBuilder().build()
+        );
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        listener = myFirebaseAuth -> {
+            FirebaseUser user = myFirebaseAuth.getCurrentUser();
+            if ( user != null)
+                //delaySplashScreen();
+                Toast.makeText(this, "Bienvenido: "+user.getUid(), Toast.LENGTH_SHORT).show();
+            else
+                showLogin();
+
+        };
+    }
+
+
+
+    private void showLogin() {
+        AuthMethodPickerLayout authMethodPickerLayout = new AuthMethodPickerLayout
+                .Builder(R.layout.activity_login)
+                .setPhoneButtonId(R.id.btn_celular)
+                .setGoogleButtonId(R.id.btn_google)
+                .setFacebookButtonId(R.id.btn_facebook)
+                .build();
+
+        startActivityForResult(AuthUI.getInstance()
+        .createSignInIntentBuilder()
+        .setAuthMethodPickerLayout(authMethodPickerLayout)
+        .setIsSmartLockEnabled(false)
+                .setTheme(R.style.LoginTheme)
+        .setAvailableProviders(providers)
+        .build(),LOGIN_REQUEST_CODE);
+    }
+
+    private void delaySplashScreen() {
+        Completable.timer(5, TimeUnit.SECONDS,
+                AndroidSchedulers.mainThread())
+                .subscribe(new Action() {
+                    @Override
+                    public void run() throws Exception {
+
+                        //
+                        firebaseAuth.addAuthStateListener(listener);
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == LOGIN_REQUEST_CODE){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK){
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            }else{
+                Toast.makeText(this, "[Error]: "+response.getError().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
